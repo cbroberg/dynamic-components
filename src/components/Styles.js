0dc121a4a58a65e58aae9ad20acb45edb01ad27b
Christian Broberg <cb@webhouse.dk>
2017-08-29 21:14:34 +0200

    Moving styles to new styled component

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -1,52 +1,30 @@
 import styled from 'styled-components'
-import * as colors from 'components/colors'
-import { getColor } from 'components/colors'
-import { transparentize, darken } from 'polished'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 export const Divider = styled.div`
     clear: both;
 `
 
-export const DisplayIcon = styled.div`
-	float: left;
-	display: inline-block;
-	text-align: center;
-    width: 200px;
-    height: 170px;
-    margin-right: 5px;
-    margin-bottom: 5px;
-	/* padding: 10px 20px 0px 0px */ /* Top, Right, Bottom, Left */
-    border: 1px solid rgba(0, 0, 0, .2);
-	background: #ECF0F1;
+export const StyledToast = styled(ToastContainer) `
+	&.toastify { 
+		.toastify-content {
+			border-radius: 4px;
+			background: #4CAF50;
+			color: #fff;
+			font-family: 'Source Sans Pro';
+			font-weight: 300;
+			font-size: 16px;
+			font-style: 'normal';
+			border: 1px rgba(0,0,0,0.2); }
+		.toastify-content--success {
+			background: #4CAF50; }
+		.toastify-content--error {
+    		background: #CE1D1D; }
+	}
+		.toastify__progress--default {
+			background: linear-gradient(to right, #2C3E50, #2C3E50, #2C3E50, #2C3E50, #2C3E50, #2C3E50); }
+	&.toastify-content--error {
+    	background: #CE1D1D; }
 `
 
-export const CopyButton = styled.button`
-    border-radius: 4px;
-    font-family: 'Source Sans Pro';
-    font-size: 14px;
-    color: ${getColor('BLACK')};
-    background: ${props => props.color || colors.BUTTON_DEFAULT};
-    margin-right: 0.7rem;
-    border: 1px solid rgba(0, 0, 0, .2);
-    text-decoration: none;
-
-    &:focus {
-        border-color: ${getColor('BUTTON_DEFAULT_FOCUS')};
-        box-shadow: 0 0 0 3px ${transparentize(0.7, colors.BUTTON_DEFAULT)};
-        outline: none;
-    }
-
-    &&:active {
-        background: ${darken(0.1, colors.BUTTON_DEFAULT)};
-        border-color: ${darken(0.1, colors.BUTTON_DEFAULT)};
-        box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
-        transform: translateY(2px);
-    }
-
-    &:hover {
-        background: ${colors.BUTTON_DEFAULT_HOVER};
-    }
-`
-
-export const LogoImg = styled.img`
-`
