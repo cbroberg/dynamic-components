0b0e2ba99e31dbadb873b4274ef1b9dc1a577bf3
Christian Broberg <cb@webhouse.dk>
2017-08-30 01:54:38 +0200

    New IconCard component allowing icon on the card

---
+++ src/components/IconCard/IconCard.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { StyledIconCard, StyledIconCardContainer, StyledIconCardHeader, StyledIconCardContent, StyledIconCardIcon } from 'components/IconCard/StyledIconCard'
+import Icon from 'components/Icon/Icon'
+
+class IconCard extends Component {
+	
+	renderIconCard() {
+		const { icon, header, content, tags } = this.props
+		return (
+			<div>
+				<StyledIconCard>
+					<StyledIconCardIcon onClick={this.props.onClick}>
+						<Icon icon={icon} size={100} />
+					</StyledIconCardIcon>
+					<StyledIconCardContainer>
+						<StyledIconCardHeader>{header}</StyledIconCardHeader>
+						<StyledIconCardContent>{'Tags: ' + tags.toString().split(',').join(', ')}</StyledIconCardContent>
+						<StyledIconCardContent>{'Usage: ' + content}</StyledIconCardContent>						
+					</StyledIconCardContainer>
+				</StyledIconCard>
+			</div>
+		)
+	}
+
+	render() {		
+		return (
+			<div>
+				{this.renderIconCard()}
+			</div>
+		)
+	}
+}
+
+IconCard.propTypes = {
+	icon: PropTypes.string,
+	header: PropTypes.string,
+	content: PropTypes.string,
+	tags: PropTypes.array
+}
+
+IconCard.defaultProps = {
+	icon: '',
+	header: 'Your header goes here',
+	content: 'Your content goes here ...',
+	tags: []
+}
+
+export default IconCard
+
