29e07e0c10954764b2e164a4fe67bd7eca2c5e64
lichondk <lichondk@gmail.com>
2017-09-11 21:04:16 +0200

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { StyledDropdown } from 'components/Dropdown/DropdownStyles'
-
+import { DropdownContianer, DropdownMenuBtn, DropdownContentContainer, SingleLinkContainer, LinkChildrenContainer, SlIconContainer, NotificationTopContanoiner, NotificationsLbl, MarkAsReadBtn, SettingsLink, SeeAllNotificationsLink, AllLinksContainer } from 'components/Dropdown/DropdownStyles'
+import * as colors from 'theme/colors'
+import Icon from './../Icon/Icon'
+import theme from 'theme/theme'
 
 class Dropdown extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			listVisible: false,
+		}
+		this.handleDropDown = this.handleDropDown.bind(this)
+	}
+
+	handleDropDown() {
+		this.state.listVisible ? this.setState({ listVisible: false }) : this.setState({ listVisible: true })
+	}
+
 
 	render() {
-		const { myProp } = this.props
 		return (
-			<StyledDropdown myProp={myProp}>{this.props.children}
-			</StyledDropdown>
+			<DropdownContianer>
+				<DropdownMenuBtn onClick={this.handleDropDown} theme={this.props.dropDowntheme} color={this.props.color}>
+					<Icon icon={this.props.icon} size={this.props.size} color={colors.BUTTON_TEXT} active={this.props.active} style={theme.iconButtonStyle} />
+					{this.props.label}
+				</DropdownMenuBtn>
+				{this.state.listVisible ?
+					<DropdownContentContainer onClick={this.props.onClick} width={this.props.width} theme={this.props.bordertheme} bgColor={this.props.bgColor}>
+						{this.props.notify ?
+							<NotificationTopContanoiner>
+								<NotificationsLbl><b>{this.props.subLabel}</b></NotificationsLbl>
+								<SettingsLink href='#'>{this.props.link}</SettingsLink>
+								<MarkAsReadBtn>{this.props.buttontext}</MarkAsReadBtn>
+							</NotificationTopContanoiner>
+							: null}
+						<AllLinksContainer theme={this.props.theme}>
+							{this.props.children}
+						</AllLinksContainer>
+						{this.props.notify ?
+							<div>
+								<SeeAllNotificationsLink href={'#'}>{this.props.seeAllNotifications}</SeeAllNotificationsLink>
+							</div>
+							: null}
+					</DropdownContentContainer> :
+					null}
+			</DropdownContianer>
 		)
 	}
 }
