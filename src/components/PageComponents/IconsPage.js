bc7511c609bf9d6fe0231dcb14ee99b0be3e01b8
KAsper Nielsen <kasper8791@gmail.com>
2017-09-13 11:52:48 +0200

    kasper

--- src/components/PageComponents/IconsPage.js
+++ src/components/PageComponents/IconsPage.js
@@ -3,8 +3,37 @@ import { copyToClipboard } from 'helpers'
 import IconCard from 'components/IconCard/IconCard'
 import { icons } from 'components/Icon/Icon'
 
-
+const newArray = []
 class IconsPage extends Component {
+	pushToNewArray() {
+		Object.entries(icons).map((item, index) => {
+			return item[1].tags.map((tag) => (
+				newArray.push(tag)
+			))
+		})
+	}
+	renderDropDownTags() {
+		this.pushToNewArray()
+		 var array = [...new Set(newArray)]
+		console.log(array)
+		 return array.map((item, index) => (
+			 <option onClick={this.renderIcons(item)} value={item}>{item}</option>	 
+		 ))
+	}
+	renderOnClick() {
+		return Object.entries(icons).map((icon, index) => {
+			return (
+				<IconCard
+					key={index}
+					icon={icon[1].name}
+					header={icon[1].action}
+					content={icon[1].usage}
+					tags={icon[1].tags}
+					onClick={(e) => copyToClipboard(e, icon[1].name, true)}
+				/>
+			)
+		})
+	}
 
 	renderIcons() {
 		return Object.entries(icons).map((icon, index) => {
@@ -20,10 +49,14 @@ class IconsPage extends Component {
 			)
 		})
 	}
-
 	render() {
 		return (
 			<div>
+				<select>
+					{this.renderDropDownTags()}
+				</select>
+				<a href={'https://material.io/icons/'} target={'_new'}>Missing an icon? Fin it here: https://material.io/icons/</a>
+				<p />
 				{this.renderIcons()}
 			</div>
 		)
@@ -31,4 +64,3 @@ class IconsPage extends Component {
 }
 
 export default IconsPage
-
