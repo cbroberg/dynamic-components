fdae621b8ea5f119c41bf46ace98e87bf31e85b3
Christian Broberg <cb@webhouse.dk>
2017-09-13 15:37:07 +0200

    Moved iconnames out of Icon folder

---
+++ src/_tmp/iconnames.js
@@ -0,0 +1,112 @@
+// All available icons are listed in this file. Constant values are in small caps and 
+// follow the Material Design icon font name specification.  
+// Component names use MdSettings where settings have a caps initial letter
+
+// Framework icons
+
+export const ICON_SEARCH = 'search' // Use for search bars and search fields
+export const ICON_MENU = 'menu' // Use for menu open/close
+export const ICON_HELP = 'help' // Use for launching the help system and support
+export const ICON_BUG_REPORT = 'bug_report' // Use for reporting bugs
+
+// Scene/Menu/App icons
+export const ICON_ASSIGNMENT = 'assignment' // Use for listing reports
+export const ICON_ASSIGNMENT_TURNED_IN = 'assignment_turned_in' // Use for reports menu
+export const ICON_INFO = 'info' // Use for displaying information
+export const ICON_DASHBOARD = 'dashboard' // Use for launching the dashboard
+export const ICON_HOME = 'home' // Use for launching the homepage
+export const ICON_SETTINGS = 'settings' // Use for displaying settings
+export const ICON_INPUT = 'input' // Use for field input
+export const ICON_TIMELINE = 'timeline' // Use for workflow
+export const ICON_CODE = 'code' // Use for workflow step automation scripts
+export const ICON_OPACITY = 'opacity' // Use for design settings
+export const ICON_PEOPLE = 'people' // Use for displaying the organisation network
+export const ICON_GROUP_ADD = 'group_add' // Use for adding a group
+export const ICON_PERSON = 'person' // Use for viewing profile information/account
+export const ICON_PERSON_ADD = 'person_add' // Use for adding a person or connection
+export const ICON_MAIL_OUTLINE = 'mail_outline' // Use for sending e-mail or e-mail settings
+export const ICON_SETTINGS_CELL = 'settings_cell' // Use for smartphone settings
+export const ICON_EXTENSION = 'extension' // Use for app modules or extensions
+export const ICON_ARROW_DROP_DOWN = 'arrow_drop_down' // Use for dropdown (alternative)
+export const ICON_ARROW_DROP_UP = 'arrow_drop_up' // Use for closing dropdown (alternative)
+export const ICON_COPYRIGHT = 'copyright' // Use for copyright
+export const ICON_CLOUD = 'cloud' //  Use for viewing your cloud storage
+export const ICON_CLOUD_DOWNLOAD = 'cloud_download' // Use for downloading data and assets 
+export const ICON_CLOUD_UPLOAD = 'cloud_upload' // Use for uploading files and assets
+export const ICON_LANGUAGE = 'language' // Use for language selection
+export const ICON_DATE_RANGE = 'date_range' // Use for selecting a date range
+export const ICON_MODE_EDIT = 'mode_edit' // Use for edit field/data/asset
+export const ICON_PLACE = 'place' // Use for map view and position/location
+export const ICON_PHONE = 'phone' // Use for calling or user profile phone details
+export const ICON_VISIBILITY = 'visibility' // Use for allow viewing or selecting fields for a list
+export const ICON_VISIBILITY_OFF = 'visibility_off' // Use for disallow viewing
+export const ICON_ACCOUNT_BOX = 'account_box' // Use for editing your account or displaying account information
+export const ICON_INSERT_DRIVE_FILE = 'insert_drive_file' // Use for managing storage
+export const ICON_VIEW_HEADLINE = 'view_headline' // Use for list view (alternative)
+export const ICON_VIEW_MODULE = 'view_module' // Use for selecting card (grid) view
+export const ICON_SHARE = 'share' // Use for sharing data or assets
+export const ICON_LABEL = 'label' // Use for tags
+export const ICON_LABEL_OUTLINE = 'label_outline'// Use for tags
+export const ICON_STAR = 'star' // Use for favourites or rating field type
+export const ICON_STAR_BORDER = 'star_border' // Use for before and after favourite and rating
+export const ICON_STARS = 'stars' // Use for rating
+export const ICON_TODAY = 'today' // Use for selecting today in a calendar view 
+export const ICON_PLAY_CIRCLE_FILLED = 'play_circle_filled' // Use for publishing a form or a page
+export const ICON_PLAY_CIRCLE_OUTLINE = 'play_circle_outline' // Use for publishing a form or a page (alternative)
+export const ICON_SKIP_NEXT = 'skip_next' // Use for navigating to the end
+export const ICON_SKIP_PREVIOUS = 'skip_previous' // Use for navigating to the begining
+export const ICON_PIE_CHART = 'pie_chart' // Use for statistics (dashboards)
+export const ICON_PIE_CHART_OUTLINED = 'pie_chart_outlined' // Use for statistics (dashboards)
+export const ICON_SHOW_CHART = 'show_chart' // Use for setting up workflow
+export const ICON_INSERT_CHART = 'insert_chart' // Use for statistics (dashboards)
+export const ICON_EQUALIZER = 'equalizer' // Use for statistics (dashboards)
+export const ICON_WEB_ASSET = 'web_asset' // Use for setting screen size
+export const ICON_WEB = 'web' // Use for web layout settings
+export const ICON_TABLET_MAC = 'tablet_mac' // Use for tablet vieport
+export const ICON_LAPTOP_MAC = 'laptop_mac' // Use for laptop viewport
+export const ICON_DESKTOP_MAC = 'desktop_mac' // Use for desktop viewport
+export const ICON_PHONE_IPHONE = 'phone_iphone' // Use for smartphone viewport
+export const ICON_PHOTO_CAMERA = 'photo_camera' // Use for taking a picture or uploading a picture
+export const ICON_SHORT_TEXT = 'short_text' // Short text paragraph 
+export const ICON_EVENT = 'event' // Use for selecting or viewing events/dates
+export const ICON_LIST = 'list' // Use for list view (alternative)
+export const ICON_WORK = 'work' // Use for business relations
+export const ICON_ACCESS_TIME = 'access_time' // Use for selecting or setting the time
+export const ICON_MIC = 'mic' // Use for recording sound
+export const ICON_VIDEOCAM = 'videocam' // Use for recording video
+export const ICON_CALL = 'call' // Use for phone call
+export const ICON_MESSAGE = 'message' // Use for launching the messaging system or messages
+export const ICON_FILTER_LIST = 'filter_list' // Use for filtering views
+export const ICON_ATTACH_FILE = 'attach_file' // Use for file attachments
+export const ICON_DIRECTIONS = 'directions' // Use for directions
+export const ICON_MOOD = 'mood' // Use for happy situations
+export const ICON_CHECK_BOX = 'check_box' // Use for check boxes
+export const ICON_RADIO_BUTTON_CHECKED = 'radio_button_checked' // Use for creating new report and radio buttons
+export const ICON_GRID_ON = 'grid_on' // Use for viewing a grid
+export const ICON_LAYERS = 'layers' // Use for layers (design)
+export const ICON_SYNC = 'sync' // Use for start synchronization
+export const ICON_SYNC_DISABLED = 'sync_disabled' // Use for disabling synchronization
+export const ICON_SYNC_PROBLEM = 'sync_problem' // Use for problems with synchronization
+
+// Login icons
+export const ICON_LOCK = 'lock_outline' // Use for login state logged in
+export const ICON_LOCK_OPEN = 'lock_open' // Use for login state logged out 
+
+// Navigation icons
+export const ICON_CLOSE = 'close' // Use for close
+export const ICON_APPS = 'apps' // Use for access to other apps
+export const ICON_KEYBOARD_ARROW_DOWN = 'keyboard_arrow_down' // Use for dropdown menu 
+export const ICON_KEYBOARD_ARROW_UP = 'keyboard_arrow_up' // Use to close dropdown menu
+export const ICON_KEYBOARD_ARROW_LEFT = 'keyboard_arrow_left' // Use for page pagination to previous
+export const ICON_KEYBOARD_ARROW_RIGHT = 'keyboard_arrow_right' // Use for page pagination to next
+export const ICON_REFRESH = 'refresh' // Use for refreshing a view
+
+// Action icons
+export const ICON_CHECK_CIRCLE = 'check_circle' // Use for check or okay in modal views
+export const ICON_CHECK = 'check' // Use for check or okay in modal views (alternative) 
+export const ICON_CANCEL = 'cancel' // Use for cancelling or closing a view or a task
+export const ICON_ADD_CIRCLE = 'add_circle'  // Use for adding new stuff (anything) 
+export const ICON_ADD_CIRCLE_OUTLINE = 'add_circle_outline' // Use for adding new stuff (alternative) 
+export const ICON_DELETE = 'delete' // Use for deleting data/assets
+export const ICON_DELETE_FOREVER = 'delete_forever' // Use for deleting data/assets permanently 
+export const ICON_CONTENT_COPY = 'content_copy' // Use for copying data/content
