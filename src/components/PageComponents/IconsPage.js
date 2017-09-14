ea9379899013e3979860335103f1723f3082d45b
KAsper Nielsen <kasper8791@gmail.com>
2017-09-14 14:26:01 +0200

    IconsPage, mangler re render med nye tags

--- src/components/PageComponents/IconsPage.js
+++ src/components/PageComponents/IconsPage.js
@@ -5,6 +5,22 @@ import { icons } from 'components/Icon/Icon'
 
 const newArray = []
 class IconsPage extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			value: ''
+		}
+		this.renderOnClick = this.renderOnClick.bind(this)
+		this.renderDropDownTags = this.renderDropDownTags.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+		this.filter = this.filter.bind(this)
+
+	}
+	filter(item) {
+		item = this.state.value
+		return newArray.filter(word => word === item)
+	}
+
 	pushToNewArray() {
 		Object.entries(icons).map((item, index) => {
 			return item[1].tags.map((tag) => (
@@ -12,52 +28,82 @@ class IconsPage extends Component {
 			))
 		})
 	}
+	renderOnClick(item) {
+		item = this.state.value
+		if (item) {
+			console.log(item)
+			return Object.entries(icons).map((icon, index) => {
+				if (this.filter(icon)) {
+					console.log('hello')
+					return (
+						<IconCard
+							key={index}
+							icon={icon[1].name}
+							header={icon[1].action}
+							content={icon[1].usage}
+							tags={['dsjdhsj']}
+							onClick={(e) => copyToClipboard(e, icon[1].name, true)}
+						/>
+					)
+				 } else {
+					console.log('Error')
+					return null
+					
+				 }
+			
+			})
+		} else {
+			return Object.entries(icons).map((icon, index) => {
+				return (
+					<IconCard
+						key={index}
+						icon={icon[1].name}
+						header={icon[1].action}
+						content={icon[1].usage}
+						tags={icon[1].tags}
+						onClick={(e) => copyToClipboard(e, icon[1].name, true)}
+					/>
+				)
+			})
+		}
+
+	}
 	renderDropDownTags() {
 		this.pushToNewArray()
-		 var array = [...new Set(newArray)]
-		console.log(array)
-		 return array.map((item, index) => (
-			 <option onClick={this.renderIcons(item)} value={item}>{item}</option>	 
-		 ))
-	}
-	renderOnClick() {
-		return Object.entries(icons).map((icon, index) => {
+		var array = [...new Set(newArray)]
+		return array.map((item, index) => {
 			return (
-				<IconCard
-					key={index}
-					icon={icon[1].name}
-					header={icon[1].action}
-					content={icon[1].usage}
-					tags={icon[1].tags}
-					onClick={(e) => copyToClipboard(e, icon[1].name, true)}
-				/>
-			)
+				<option key={index} value={item}>{item}</option>)
 		})
 	}
 
-	renderIcons() {
-		return Object.entries(icons).map((icon, index) => {
-			return (
-				<IconCard
-					key={index}
-					icon={icon[1].name}
-					header={icon[1].action}
-					content={icon[1].usage}
-					tags={icon[1].tags}
-					onClick={(e) => copyToClipboard(e, icon[1].name, true)}
-				/>
-			)
-		})
+	handleChange(event) {
+		this.setState({ value: event.target.value })
+
 	}
+	// renderIcons() {
+	// 	return Object.entries(icons).map((icon, index) => {
+	// 		return (
+	// 			<IconCard
+	// 				key={index}
+	// 				icon={icon[1].name}
+	// 				header={icon[1].action}
+	// 				content={icon[1].usage}
+	// 				tags={icon[1].tags}
+	// 				onClick={(e) => copyToClipboard(e, icon[1].name, true)}
+	// 			/>
+	// 		)
+	// 	})
+	// }
 	render() {
 		return (
 			<div>
-				<select>
+				<select value={this.state.value} onChange={this.handleChange}>
 					{this.renderDropDownTags()}
 				</select>
 				<a href={'https://material.io/icons/'} target={'_new'}>Missing an icon? Fin it here: https://material.io/icons/</a>
 				<p />
-				{this.renderIcons()}
+				{this.renderOnClick()}
 			</div>
 		)
 	}
