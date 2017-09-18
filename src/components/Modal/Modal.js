460df9543889a9e9a396c4b34da52e1e9cc6653e
Christian Broberg <cb@webhouse.dk>
2017-09-18 20:49:18 +0200

    Updated now with embedded onRequestClose for content element

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,8 +9,8 @@ class Modal extends Component {
 
 
 	render() {
-		/* Props */
-		const { isOpen, onAfterOpen, onRequestClose, closeTimeoutMS, style, content, icon, label } = this.props
+
+		const { isOpen, onAfterOpen, onRequestClose, closeTimeoutMS, style, content, icon, label, shouldCloseOnOverlayClick } = this.props
 
 		return (
 			<div>
@@ -20,7 +20,8 @@ class Modal extends Component {
 					closeTimeoutMS={closeTimeoutMS} 
 					onRequestClose={onRequestClose} 
 					contentLabel={label}
-					style={style}>
+					style={style}
+					shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}>
 					
 					<StyledModalHeader>
 				
@@ -30,14 +31,14 @@ class Modal extends Component {
 				
 						<StyledModalHeaderTitle>{label}</StyledModalHeaderTitle>
 				
-						<StyledModalHeaderClose onClick={(e) => { e.preventDefault(); onRequestClose() }}>
+						<StyledModalHeaderClose onClick={onRequestClose}>
 							<Icon icon={'close'} iconSize={30} color={colors.MODAL_ICON} active={true} />
 						</StyledModalHeaderClose>
 				
 					</StyledModalHeader>
 					
 					<StyledModalContent>
-						{content}
+						{content ? content : this.defaultProps.content}
 					</StyledModalContent>
 				
 				</StyledModal>
@@ -47,11 +48,24 @@ class Modal extends Component {
 }
 
 Modal.propTypes = {
-	myProp: PropTypes.string
+	isOpen: PropTypes.bool.isRequired,
+	onAfterOpen: PropTypes.func,
+	onRequestClose: PropTypes.func,
+	closeTimeoutMS: PropTypes.number,
+	style: PropTypes.shape({
+		overlay: PropTypes.object,
+		content: PropTypes.object
+	}),
+	content: PropTypes.any,
+	icon: PropTypes.string,
+	label: PropTypes.string.isRequired,
+	shouldCloseOnOverlayClick: PropTypes.bool
 }
 
 Modal.defaultProps = {
-	myProp: ''
+	isOpen: false,
+	label: '',
+	content: 'You forgot to put some content here ...'
 }
 
 export default Modal
--- src/components/PageComponents/ModalPage.js
+++ src/components/PageComponents/ModalPage.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import Modal from 'components/Modal/Modal'
 
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+		fontSize: '150px'
+	}
+}
+
 class ModalPage extends Component {
 
 	constructor(props) {
@@ -26,21 +38,23 @@ class ModalPage extends Component {
 		return (
 			<div>
 				<button onClick={this.openModal}>Open Modal Window</button>
-
+				
 				<Modal 
 					isOpen={this.state.modalIsOpen} 
 					onRequestClose={this.onRequestClose} 
 					content={<Content onRequestClose={this.onRequestClose} firstName={'Christian'} lastName={'Broberg'}/>} 
 					icon={'star'} 
 					label={'This is a modal window'} 
+					style={customStyles}
 				/>
-				
+
 			</div>
 		)
 	}
 }
 
 export default ModalPage
+
 
 const Content = (props) => {
 	return (
@@ -49,12 +63,11 @@ const Content = (props) => {
 			<div>
 				<p>Here is a list:</p>
 				<ul>
-					<li>Item 1</li>
-					<li>Item 2</li>
+					<li>Modal Window Item 1</li>
+					<li>Modal Window Item 2</li>
 				</ul>
 				<button onClick={props.onRequestClose}>Close Modal Window</button>
 			</div>
 		</div>
 	)
 }
-
