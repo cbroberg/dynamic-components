867b8782ea39fdd1ca9d33a5e5651be64fda77c4
Frida Jakobsen <1054351@ucn.dk>
2017-09-21 09:30:38 +0100

    3

--- src/components/Skeleton/AllFormsCard.js
+++ src/components/Skeleton/AllFormsCard.js
@@ -6,48 +6,48 @@ class AllFormsCard extends Component {
 	render() {
 		return (
 			<div>
-				{/* #1 */}
+				{/* Style #1 */}
 				<FormBox>
 					<AnimatedTop/>
 					<AnimatedMiddle>
-						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="16.5px"/><br/>
-						<SingleLineLoader height="9px" width="113.29px" marginLeft="15px" marginTop="4.5px"/><br/>
+						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="16.5px"/>
+						<SingleLineLoader height="9px" width="113.29px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="9px" width="133.29px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="11.33px" width="157.29px" marginTop="20px" marginBottom="7.5px" marginRight="15px" marginLeft="15px" borderRadius="2px"/>
 					</AnimatedMiddle>
 					<AnimatedBottom/>
                 </FormBox>
                 
-				{/* #2 */}
+				{/* Style #2 */}
 				<FormBox>
 					<AnimatedTop/>
 					<AnimatedMiddle>
-						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="16.5px"/><br/>
-						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="4.5px"/><br/>
+						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="16.5px"/>
+						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="11.33px" width="157.29px" marginTop="20px" marginBottom="7.5px" marginRight="15px" marginLeft="15px" borderRadius="2px"/>
 					</AnimatedMiddle>
 					<AnimatedBottom/>
                 </FormBox>
                 
-				{/* #3 */}
+				{/* Style #3 */}
 				<FormBox>
 					<AnimatedTop/>
 					<AnimatedMiddle>
-						<SingleLineLoader height="9px" width="130px" marginLeft="15px" marginTop="16.5px"/><br/>
-						<SingleLineLoader height="9px" width="145px" marginLeft="15px" marginTop="4.5px"/><br/>
+						<SingleLineLoader height="9px" width="130px" marginLeft="15px" marginTop="16.5px"/>
+						<SingleLineLoader height="9px" width="145px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="9px" width="90px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="11.33px" width="157.29px" marginTop="20px" marginBottom="7.5px" marginRight="15px" marginLeft="15px" borderRadius="2px"/>
 					</AnimatedMiddle>
 					<AnimatedBottom/>
                 </FormBox>
                 
-				{/* #4 */}
+				{/* Style #4 */}
 				<FormBox>
 					<AnimatedTop/>
 					<AnimatedMiddle>
-						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="16.5px"/><br/>
-						<SingleLineLoader height="9px" width="130px" marginLeft="15px" marginTop="4.5px"/><br/>
+						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="16.5px"/>
+						<SingleLineLoader height="9px" width="130px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="9px" width="157.29px" marginLeft="15px" marginTop="4.5px"/>
 						<SingleLineLoader height="11.33px" width="157.29px" marginTop="20px" marginBottom="7.5px" marginRight="15px" marginLeft="15px" borderRadius="2px"/>
 					</AnimatedMiddle>
