95befe5a42d62b2c88e9f5148a47a4d00592420b
Christian Broberg <cb@webhouse.dk>
2017-10-06 23:53:09 +0200

    Added debugging info to all examples with ButtonPanel and removing iconsize (default props) and toast notifier

--- src/components/PageComponents/ButtonsPanelPage.js
+++ src/components/PageComponents/ButtonsPanelPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { copyToClipboard } from 'helpers'
 import Button from 'components/Button/Button'
 import ButtonPanel from 'components/ButtonPanel/ButtonPanel'
 import styled from 'styled-components'
@@ -14,205 +13,157 @@ class ButtonsPage extends Component {
 	render() {
 		return (
 			<div>
-				Panel (direction row default props) with content, default props
+				ButtonPanel (direction row default props) with content component array, default props
 				<Div flex={'column nowrap'} >
 					<ButtonPanel justify='left' debug={true} content={[
 						<Button
 							label='Button 1 with a long name'
 							icon='mail_outline'
-							iconSize={18}
 							color={'#3B97D3'}
-							onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 						/>,
 						<Button
 							label='Button 2 with a long name'
 							icon='settings'
-							iconSize={18}
 							color={'#3B97D3'}
-							onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 						/>,
 						<Button
 							label='Button 3 with a long name'
 							icon='menu'
-							iconSize={18}
 							color={'#3B97D3'}
-							onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 						/>]
 					} />
 					<ButtonPanel justify='right' debug={true} content={[
 						<Button
 							label='Button 1'
 							icon='mail_outline'
-							iconSize={18}
 							color={'#3B97D3'}
-							onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 						/>,
 						<Button
 							label='Button 2'
 							icon='settings'
-							iconSize={18}
 							color={'#3B97D3'}
-							onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 						/>,
 						<Button
 							label='Button 3'
 							icon='menu'
-							iconSize={18}
 							color={'#3B97D3'}
-							onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 						/>]
 					} />
 				</Div>
 				<div>
-					Panel Row(default) with Children, default props
+					ButtonPanel (direction row default) with children, default props
 				<Div flex={'column nowrap'}>
-						<ButtonPanel>
+						<ButtonPanel debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
 						<ButtonPanel debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
 					</Div>
 				</div>
 				<div>
-					Panel Column with Children, default props
+					ButtonPanel (direction column) with children, default props
 					<Div flex='row nowrap'>
-						<ButtonPanel direction='column' height='auto' width='auto'>
+						<ButtonPanel direction='column' height='auto' width='auto' debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
-						<ButtonPanel direction='column' debug={true} height='auto' width='auto'>
+						<ButtonPanel direction='column' height='auto' width='auto' debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
 					</Div>
 				</div>
 				<div>
-					Panel Column with content
+					ButtonPanel (direction column) with content component array
 					<Div flex='row nowrap'>
-						<ButtonPanel direction='column'
+						<ButtonPanel direction='column' debug={true}
 							content={[
 								<Button
 									label='Button 1'
 									icon='mail_outline'
-									iconSize={18}
-									color={'#3B97D3'}
-									onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
+									color={'#3B97D3'}	
 								/>,
 								<Button
 									label='Button 2'
 									icon='settings'
-									iconSize={18}
-									color={'#3B97D3'}
-									onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
+									color={'#3B97D3'}	
 								/>,
 								<Button
 									label='Button 3'
 									icon='menu'
-									iconSize={18}
-									color={'#3B97D3'}
-									onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
+									color={'#3B97D3'}	
 								/>]
 							} />
 						<ButtonPanel direction='column' justify='left' debug={true} content={[
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>,
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>,
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>]
 						}>
 
@@ -220,105 +171,81 @@ class ButtonsPage extends Component {
 					</Div>
 				</div>
 				<div>
-					Panel Row with Children, justify space-between
+					ButtonPanel (direction row) with children, justify space-between
 					<Div flex='row nowrap' style={{ width: '100%' }}>
-						<ButtonPanel direction='row' width={'100%'} justify='spaceBetween'>
+						<ButtonPanel direction='row' width={'100%'} justify='spaceBetween' debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
-						<ButtonPanel direction='row' debug={true} justify={'spaceBetween'} width={'100%'}>
+						<ButtonPanel direction='row' justify={'spaceBetween'} width={'100%'} debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
 					</Div>
 				</div>
 				<div>
-					Panel Column with content
+					ButtonPanel (direction column) with content component array
 					<Div flex='row nowrap'>
-						<ButtonPanel direction='row' justify={'spaceBetween'} width={'100%'}
+						<ButtonPanel direction='row' justify={'spaceBetween'} width={'100%'} debug={true}
 							content={[
 								<Button
 									label='Button 1'
 									icon='mail_outline'
-									iconSize={18}
-									color={'#3B97D3'}
-									onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
+									color={'#3B97D3'}	
 								/>,
 								<Button
 									label='Button 2'
 									icon='settings'
-									iconSize={18}
-									color={'#3B97D3'}
-									onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
+									color={'#3B97D3'}	
 								/>,
 								<Button
 									label='Button 3'
 									icon='menu'
-									iconSize={18}
-									color={'#3B97D3'}
-									onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
+									color={'#3B97D3'}	
 								/>]
 							} />
-						<ButtonPanel direction='row' debug={true} justify={'spaceBetween'} width={'100%'}
+						<ButtonPanel direction='row' justify={'spaceBetween'} width={'100%'} debug={true} 
 						 content={[
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>,
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>,
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>]
 						}>
 
@@ -326,52 +253,40 @@ class ButtonsPage extends Component {
 					</Div>
 				</div>
 				<div>
-					Panel Row with Children, justify right
+					ButtonPanel (direction row) with children, justify right
 					<Div flex='row nowrap' style={{ width: '100%' }}>
-						<ButtonPanel direction='row' width={'100%'} justify='right'>
+						<ButtonPanel direction='row' width={'100%'} justify='right' debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
-						<ButtonPanel direction='row' debug={true} justify={'right'} width={'100%'}>
+						<ButtonPanel direction='row' justify={'right'} width={'100%'} debug={true}>
 							<Button
 								label='Button 1'
 								icon='mail_outline'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 2'
 								icon='settings'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 							<Button
 								label='Button 3'
 								icon='menu'
-								iconSize={18}
 								color={'#3B97D3'}
-								onClick={(e) => copyToClipboard(e, 'Mail is now send', true)}
 							/>
 						</ButtonPanel>
 					</Div>
