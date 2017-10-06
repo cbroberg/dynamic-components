a62e8ea035cf02df8de1c421f10da5b4e557e4a9
Christian Broberg <cb@webhouse.dk>
2017-10-06 22:52:00 +0200

    Code style updates

--- src/components/ButtonPanel/ButtonPanelStyles.js
+++ src/components/ButtonPanel/ButtonPanelStyles.js
@@ -13,20 +13,20 @@ const justify = {
 }
 
 const StyledButtonPanel = styled.div`
-display:flex;
-overflow:hidden;
-flex-flow: ${props => props.direction + ' ' + props.wrap};
-justify-content: ${props => justify[props.justify]};
-align-items: ${props => alignment[props.align]};
-width: ${(props) => props.width};
-height: ${props => props.height};
-border: ${props => props.debug ? '1px solid black' : ''};
-&:first-child div{
-	margin-left:${props => (props.direction === 'row' && props.justify === 'left') ? "0px" : ""};
-}
-&:last-child div{
-	margin-right:${props => (props.direction === 'row' && props.justify === 'right') ? "0px" : ""};
-}
+	display: flex;
+	overflow: hidden;
+	flex-flow: ${props => props.direction + ' ' + props.wrap};
+	justify-content: ${props => justify[props.justify]};
+	align-items: ${props => alignment[props.align]};
+	width: ${(props) => props.width};
+	height: ${props => props.height};
+	border: ${props => props.debug ? '1px solid black' : ''};
+	&:first-child div {
+		margin-left:${props => (props.direction === 'row' && props.justify === 'left') ? "0px" : ""};
+	}
+	&:last-child div {
+		margin-right:${props => (props.direction === 'row' && props.justify === 'right') ? "0px" : ""};
+	}
 `
 
 export default StyledButtonPanel
