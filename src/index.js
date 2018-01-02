d56f7d3eb7556370e3eb7f69e2576e2b9b30c5de
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2018-01-02 19:05:44 +0100

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 import App from './App'
 
 require('typeface-source-sans-pro')
 
-ReactDOM.render(<App version={1} />, document.getElementById('root'))
+class AppHotLoader extends Component {
+	render() {
+		return (
+			<App version={1} />	
+		)
+	}
+}
+
+export default AppHotLoader
+
+render(
+	<AppHotLoader />,
+	document.getElementById('root')
+)
+
+if (module.hot) {
+	module.hot.accept('./index'.default, () => {
+		const NextApp = require('./index').default
+		render(
+			<NextApp />,
+			document.getElementById('root')
+		)
+	})
+}
+
+// render(<App version={1} />, document.getElementById('root'))
